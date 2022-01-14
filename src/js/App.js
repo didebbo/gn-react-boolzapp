d0ad3fd8c8b626db9e0a860f9deffcdbecdfb552
didebbo <gianluca.napoletano.93@gmail.com>
2022-01-14 14:09:19 +0100

    in lavorazione sidebar

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import '../scss/App.scss';
 import Sidebar from './components/Sidebar';
+import database from '../config/database'
 
 function App() {
+  const [data, setData] = useState(database);
   return (
     <div className="App">
       <Sidebar />
-      React App
     </div>
   );
 }
--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -3,7 +3,36 @@ import '../../scss/Sidebar.scss';
 function Sidebar() {
     return (
         <div className="Sidebar">
-            React App
+            <div className="header">
+                <div className="user">
+                    <div className="avatar">
+                        <img src={require('../../images/avatar_io.jpg')} alt="Me" />
+                    </div>
+                    <div className="name">
+                        Giada
+                    </div>
+                </div>
+                <div className="actions">
+                    <i className="fas fa-circle-notch"></i>
+                    <i className="fas fa-comment-alt"></i>
+                    <i className="fas fa-ellipsis-v"></i>
+                </div>
+            </div>
+            <div className="body">
+                <div className="notifiche">
+                    <div className="icon">
+                        <i className="fas fa-bell-slash"></i>
+                    </div>
+                    <div className="content">
+                        <strong>
+                            Ricevi notifiche di nuovi messaggi
+                        </strong>
+                        <small>
+                            Attiva notifiche desktop
+                        </small>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
